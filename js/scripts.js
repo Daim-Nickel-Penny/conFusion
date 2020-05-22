65f8f77a03519a78f83d0234efe6577ff5cec870
daimazon <1804449@kiit.ac.in>
2020-05-22 17:16:36 +0530

    Jquery Added

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    $("#mycarousel").carousel( { interval: 2000 } );
+
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass('fa-pause');
+            $("#carouselButton").children("span").addClass('fa-play');
+        }
+        else if ($("#carouselButton").children("span").hasClass('fa-play')){
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children("span").removeClass('fa-play');
+            $("#carouselButton").children("span").addClass('fa-pause');                    
+        }
+    });
+
+});
+    $(document).ready(function(){
+                $(".sgnbtn").click(function(){
+                    $("#loginModal").modal("toggle");
+                });
+                });
+                $(document).ready(function(){
+                    $(".sgnbtn2").click(function(){
+                        $("#reservetable").modal("toggle");
+                    });
+                });
+
+                        $(document).ready(function(){
+            $(".cls").click(function(){
+                $("#loginModal").modal("hide");
+                $("#reservetable").modal("hide");
+            });    
+        }); 
